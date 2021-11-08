99b370dbb276cb67acab0871ae9144eb12fe72fd
balerion <davide.curcio@gmail.com>
2021-11-08 15:27:08 +0100

    fix danish character errors in sms

--- SIM800L.py
+++ SIM800L.py
@@ -9,6 +9,10 @@ import serial
 import logging
 import time, sys, codecs
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.getLogger("omxplayer.player").setLevel(logging.CRITICAL + 1)
+logger = logging.getLogger(__name__)
 
 class SIM800L:
     def __init__(
@@ -54,13 +58,14 @@ class SIM800L:
         logging.info('Sending AT command: '+command)
         try:
             self.ser.write((command + "\r").encode())
-            received='\t'
-            received += (self.ser.read(20).splitlines()[-1]).decode()
-            logging.info(received)
+            received = [ll.decode('cp1252') for ll in self.ser.readlines()]
+            for rr in received:
+                logging.info(rr)
             if "ERROR" in received:
                 return False
             return received
-        except:
+        except Exception as e:
+            logging.error(e)
             print("Couldn't write on " + self.portName)
             return False
 
@@ -152,3 +157,8 @@ class SIM800L:
                 return 0            
 
         return success
+
+if __name__ == "__main__":
+    print("asd")
+
+#EOF
